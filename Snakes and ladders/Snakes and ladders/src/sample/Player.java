f9533a318cd84f5e24bdc625872980b1a0bc0923
iamrohan1811 <88678200+iamrohan1811@users.noreply.github.com>
2021-12-22 23:00:42 +0530

    updated movement in players

--- Snakes and ladders/Snakes and ladders/src/sample/Player.java
+++ Snakes and ladders/Snakes and ladders/src/sample/Player.java
@@ -36,22 +36,62 @@ public class Player {
 //
 //            }
 //        }
-        for(int i=0;i<dice_no;i++){
-
-            move_ahead();
+        if(!ready){
+            if(dice_no==1){
+                ready=true;
+                move_ahead();
+            }
         }
+        else{
+            for(int i=0;i<dice_no;i++){
+                move_ahead();
+            }
+        }
+
+    }
+
+    public int[] calculate(int n){
+        int q,r;
+        int[] coordinates = new int[2];
+        int i,j;
+        q=n/10;
+        r=n%10;
+        if(r==0){
+            i=q-1;
+            if(q%2==0){
+                j=0;
+            }
+            else{
+                j=9;
+            }
+        }
+        else{
+            i=q;
+            if(q%2==0){
+                j=r-1;
+            }
+            else{
+                j=9-r+1;
+            }
+        }
+        coordinates[0]=i;
+        coordinates[1]=j;
+        return coordinates;
     }
 
     public void linear_move(int prev_pos,int next_pos){
         //translate transition
         int r=0,c=0;
+        int[] coordinate1=calculate(prev_pos);
+        int[] coordinate2 =calculate(next_pos);
+        //coordinate1[0]-coordinate2[0]*tile_size+coordinate1[0]
         int x1=0,y1=0;
         int x2=20,y2=20;
         TranslateTransition translate=new TranslateTransition();
-        translate.setFromX(x1);
-        translate.setFromY(y1);
-        translate.setToX(x2);
-        translate.setToY(y2);
+        translate.setFromX(coordinate1[0]*39.2);
+        translate.setFromY(coordinate1[1]*56.8);
+        translate.setToX(coordinate2[0]*39.2);
+        translate.setToY(coordinate2[0]*56.8);
         translate.setDuration(Duration.millis(500));
         translate.setNode(player_image);
         translate.play();
